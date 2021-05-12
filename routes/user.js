94c811a1c3c7d08d3c92fc62b6482d095f1309ed
mauricio-bs <mauricio.3g2014@gmail.com>
2021-05-12 20:27:55 -0300

    Rotas de criação de usuarios concertada, e feito o redirecionamento para edição do usuario

--- routes/user.js
+++ routes/user.js
@@ -1,31 +1,35 @@
 //Rotas de usuarios
-const express = require("express");
-const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
-const passport = require("passport");
+const express = require('express')
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const passport = require('passport')
 //Controlers
-const UserCont = require("../controller/userController");
-const ticket = require("../controller/ticketController");
+const UserCont = require('../controller/userController')
+const ticket = require('../controller/ticketController')
 //Auth
-const { isAdmin } = require("../helpers/Authen");
+const { isAdmin } = require('../helpers/Authen')
 //Config
-const user = express.Router();
+const user = express.Router()
 
 //cadastro de usuario
-user.get("/register", (req, res) => {
-  res.render("admin/register");
-});
+user.get('/register', (req, res) => {
+	res.render('admin/register')
+})
 
-user.post("/register", UserCont.newUser);
+user.post('/register', UserCont.newUser)
+
+user.get('/users', (req, res) => {
+	res.render('admin/users')
+})
 
 //editar usuario
-// user.get('/Users/edit/:id8', (req, res) => {
-
-// })
+user.get('/users/edit/:id8', (req, res) => {
+	res.render('admin/editUser')
+})
 
 //deletar usuario
-user.get("/user/User/:id", isAdmin, (req, res) => {
-  res.render("admin/users");
-});
+user.get('/users/delete/:id', isAdmin, (req, res) => {
+	res.render('admin/users')
+})
 
-module.exports = user;
+module.exports = user
