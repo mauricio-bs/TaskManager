5fecd3b07fca946182dc6341cd224cfb3cc11287
mauricio-bs <mauricio.3g2014@gmail.com>
2021-05-11 22:03:00 -0300

    Códigos formatados com prettier

--- routes/admin.js
+++ routes/admin.js
@@ -1,30 +1,48 @@
 //Rotas administradoras
-const express = require('express')
-const adm = express.Router()
+const express = require("express");
+const adm = express.Router();
 //Controller
-const adminCont = require('../controller/adminController')
+const adminCont = require("../controller/adminController");
 //Autenticação
-const {isAdmin} = require('../helpers/Authen')
+const { isAdmin } = require("../helpers/Authen");
 
 //Colaboradores
-adm.get('/colaborators', isAdmin, (req, res) => {
-    res.render('admin/colaborator/colaborators')
-})
+
+//Pagina lista de colaboradores
+adm.get("/colaborators", (req, res) => {
+  res.render("admin/colaborator/colaborators");
+});
 
 //Registrar colaboradores
-adm.get('/colaborators/register', isAdmin, (req, res) => {
-    res.render('admin/colaborator/regColaborator')
-})
+adm.get("/colaborators/register", (req, res) => {
+  res.render("admin/colaborator/regColaborator");
+});
 
-adm.post('/colaborators/register', isAdmin, adminCont.regColab)
-
+adm.post("/colaborators/register", adminCont.regColab);
 
 //Editar colaborador
-adm.get('/colaborators/editColab/:_id', isAdmin, (req, res) => {
-    res.render('')
-})
+adm.get("/colaborators/editColab/:_id", (req, res) => {
+  res.render("");
+});
 
 //Buscar colaborador
 // adm.get('/colaborators/search/:id8', isAdmin, admin.searchColab)
 
-module.exports = adm
+//
+
+//Departamentos
+//Pagina de departamentos
+adm.get("/departments", (req, res) => {
+  res.render("admin/dp/departments");
+});
+
+//Cadastro de departamentos
+adm.get("/departments/new-department", (req, res) => {
+  res.render("admin/dp/regDepartment");
+});
+
+adm.post("/departments/new-department", adminCont.regDP);
+
+//Empresas
+
+module.exports = adm;
--- routes/ticket.js
+++ routes/ticket.js
@@ -1,16 +1,16 @@
 //rotas relacionadas aos atendimentos
-const express = require('express')
-const atend = express.Router()
+const express = require("express");
+const atend = express.Router();
 //controller
-const ticket = require('../controller/ticketController')
-const attendance = require('../model/attendance')
+const ticket = require("../controller/ticketController");
+const attendance = require("../model/attendance");
 //Helper
-const {Authenticated} = require('../helpers/Authen')
+const { Authenticated } = require("../helpers/Authen");
 
-atend.get('/dashboard', Authenticated , (req, res) => {
-    res.render('attendance/dashboard')
-})
+atend.get("/dashboard", Authenticated, (req, res) => {
+  res.render("attendance/dashboard");
+});
 
-atend.get('/ticket/:_id', Authenticated , attendance)
+atend.get("/ticket/:_id", Authenticated, attendance);
 
-module.exports = atend
+module.exports = atend;
